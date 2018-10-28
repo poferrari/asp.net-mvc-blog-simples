b10e2d63b7c3af39a77716492ed892157c057e5d
JOAO VITOR FERRARI DA SILVA <poferrari@gmail.com>
2018-10-28 07:46:10 -0300

    CRUD para Artigo e Autor.

---
+++ BlogPessoal.Web/Controllers/AutoresController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogPessoal.Web.Data.Contexto;
+using BlogPessoal.Web.Models.Autores;
+
+namespace BlogPessoal.Web.Controllers
+{
+    public class AutoresController : Controller
+    {
+        private BlogPessoalContexto db = new BlogPessoalContexto();
+
+        // GET: Autores
+        public ActionResult Index()
+        {
+            return View(db.Autores.ToList());
+        }
+
+        // GET: Autores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autores.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // GET: Autores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Autores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Email,Senha,Administrador,DataDeCadastro")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Autores.Add(autor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(autor);
+        }
+
+        // GET: Autores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autores.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Email,Senha,Administrador,DataDeCadastro")] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(autor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(autor);
+        }
+
+        // GET: Autores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Autor autor = db.Autores.Find(id);
+            if (autor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(autor);
+        }
+
+        // POST: Autores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Autor autor = db.Autores.Find(id);
+            db.Autores.Remove(autor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- BlogPessoal.Web/Models/CategoriasDeArtigo/CategoriaDeArtigo.cs
+++ BlogPessoal.Web/Models/CategoriasDeArtigo/CategoriaDeArtigo.cs
@@ -1,12 +1,21 @@
 ﻿using BlogPessoal.Web.Models.Artigos;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogPessoal.Web.Models.CategoriasDeArtigo
 {
     public class CategoriaDeArtigo
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
         public string Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        [Display(Name = "Descrição")]
+        [DataType(DataType.MultilineText)]        
         public string Descricao { get; set; }
 
         public virtual ICollection<Artigo> Artigos { get; set; }
