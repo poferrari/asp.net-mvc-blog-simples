3802d25838f280131066d39ddb079981d65e7bc1
JOAO VITOR FERRARI DA SILVA <poferrari@gmail.com>
2018-12-01 13:37:18 -0200

    Compressão de requisição.

--- BlogPessoal.Web/Global.asax.cs
+++ BlogPessoal.Web/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using BlogPessoal.Web.App_Start;
 using System;
+using System.IO.Compression;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -20,23 +22,27 @@ namespace BlogPessoal.Web
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            //HttpApplication app = (HttpApplication)sender;
+            HttpApplication app = (HttpApplication)sender;
+            CompressaoDeRequisicao(app);
+        }
 
-            //string encodings = app.Request.Headers.Get("Accept-Encoding");
-            //if (encodings != null)
-            //{
-            //    encodings = encodings.ToLower();
-            //    if (encodings.Contains("deflate"))
-            //    {
-            //        app.Response.Filter = new DeflateStream(app.Response.Filter, CompressionMode.Compress);
-            //        app.Response.AppendHeader("Content-Encoding", "deflate");
-            //    }
-            //    else if (encodings.Contains("gzip"))
-            //    {
-            //        app.Response.Filter = new GZipStream(app.Response.Filter, CompressionMode.Compress);
-            //        app.Response.AppendHeader("Content-Encoding", "gzip");
-            //    }
-            //}
+        private void CompressaoDeRequisicao(HttpApplication app)
+        {
+            string encodings = app.Request.Headers.Get("Accept-Encoding");
+            if (encodings != null)
+            {
+                encodings = encodings.ToLower();
+                if (encodings.Contains("deflate"))
+                {
+                    app.Response.Filter = new DeflateStream(app.Response.Filter, CompressionMode.Compress);
+                    app.Response.AppendHeader("Content-Encoding", "deflate");
+                }
+                else if (encodings.Contains("gzip"))
+                {
+                    app.Response.Filter = new GZipStream(app.Response.Filter, CompressionMode.Compress);
+                    app.Response.AppendHeader("Content-Encoding", "gzip");
+                }
+            }
         }
     }
 }
